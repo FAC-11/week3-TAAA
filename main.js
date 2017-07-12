e5e436591701a7fbc92063ad7a1453eec816cf8e
Morkeltry <morkeltry@mailfence.com>
2017-07-12 17:30:50 +0100

    made requesting function

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+ var Tfl = {
+  front: "https://api.tfl.gov.uk/journey/journeyresults/"
+  mid: "/to/"
+  end: ""};
+
 function populateMenu(menuToPopulate) {
   var menu = document.getElementById(menuToPopulate);
   var stationsArray = [];
@@ -10,6 +15,25 @@ function populateMenu(menuToPopulate) {
     menu.appendChild(menuItem);
   })
 }
+
+
+function makeRequest (requestString) {
+  var xhr = XMLHttpRequest ();
+    xhr.onreadystatechange = function() {
+      if (xhr.readystate==4 && xhr.status==200) {
+        var resp = JSON.parse (xhr.responseText);
+      }
+    };
+    xhr.open ('GET',requestString, true);
+    xhr.send ();
+    return xhr;
+  }
+
+function hasSubmitted () {
+  
+}
+
+
 
 var availableStations = Object.assign({}, stationObj);
 
