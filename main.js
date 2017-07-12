622a206d6804a444ffa18fd7c2252667d24ce4a4
AseelM <aseelmustafa@outlook.com>
2017-07-12 16:57:16 +0100

    populated dropdown menu
    relates #7

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log(stationObj);
+function populateMenu(menuToPopulate) {
+  var menu = document.getElementById(menuToPopulate);
+  var stationsArray = [];
+
+  Object.keys(availableStations).forEach(function(el) {
+    // stationsArray[el] = availableStations[el];
+    var menuItem = document.createElement("option");
+    menuItem.value = [el];
+    menuItem.textContent = availableStations[el];
+    menu.appendChild(menuItem);
+  })
+}
+
+var availableStations = Object.assign({}, stationObj);
+
+populateMenu("starting-station");
+populateMenu("destination-station");
