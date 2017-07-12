d70b755bcfb2c695042d9b66250339f1e48647eb
Morkeltry <morkeltry@mailfence.com>
2017-07-12 15:45:46 +0100

    converted long list to JSON some test code, but didn't like the strings on different lines short list still to convert

--- station-ob.js
+++ station-ob.js
@@ -1,8 +1,32 @@
 
+var stationObj={
+1000003: "ALDGATE",
+1000004: "ALDGATE EAST",
+1000013: "BANK",
+1000022: "BETHNAL GREEN",
+1001023: "BETHNAL GREEN NR",
+1002019: "BOW CHURCH",
+1000029: "BOW ROAD",
+1000138: "LIVERPOOL STREET",
+1000139: "LONDON BRIDGE",
+1000146: "MILE END",
+1000148: "MONUMENT",
+1000149: "MOORGATE",
+1000169: "OLD STREET",
+1002066: "PUDDING MILL LANE",
+1000225: "ST PAUL'S",
+1000220: "STEPNEY GREEN",
+1000226: "STRATFORD",
+1003007: "STRATFORD HIGH STREET",
+1003009: "STRATFORD INTERNATIONAL",
+1002076: "TOWER GATEWAY",
+1000238: "TOWER HILL",
+1000268: "WHITECHAPEL",
+}
 
 
 
-var stationString="1000003,ALDGATE"
+// var stationString="1000003,ALDGATE
 // 1000004,ALDGATE EAST
 // 1000013,BANK
 // 1000022,BETHNAL GREEN
@@ -25,21 +49,21 @@ var stationString="1000003,ALDGATE"
 // 1000238,TOWER HILL
 // 1000268,WHITECHAPEL
 // "
-stationsArray=stationString.split("/n");
+// stationsArray=stationString.split("/n");
+//
+// // stationsObj={};
+// stationsArray=[];
+// intermediateArray.forEach (function (stationPair) {
+//   var pair=stationPair.split(": ");
+//   if (pair[0]>=1000000 && pair[1] && pair[1].length>0) {
+//     stationsObj [pair[0]]=pair[1];
+//     stationsArray [pair[0].slice(3,6)]=pair[1];
+//   }
+//   else {
+//     console.log ('weird data found:',stationPair);
+//   }
+// });
 
-stationsObj={};
-stationsArray=[];
-intermediateArray.forEach (function (stationPair) {
-  var pair=stationPair.split(",");
-  if (pair[0]>=1000000) & pair[1] && pair[1].length>0) {
-    stationsObj [pair[0]]=pair[1];
-    stationsArray [pair[0].slice(3,6)]=pair[1];
-  }
-  else {
-    console.log ('weird data found:',stationPair);
-  }
-});
 
-
-exports.stationsArray = stationsArray;
-exports.stationsObj = stationsObj;
+// exports.stationsArray = stationsArray;
+// exports.stationsObj = stationsObj;
